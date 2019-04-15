9d7b73e7eb4c3b8c9c5d090418abce03e9f8dcb3
Rhioda muthie <rhioda.m@gmail.com>
2019-04-15 13:46:10 +0700

    done crud and adding navbar

---
+++ app/Http/Controllers/KontakController.php
@@ -0,0 +1,114 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Kontak;
+use Illuminate\Http\Request;
+
+class KontakController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+        $data = Kontak::all();
+        return view('kontak.index',compact('data'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+        return view('kontak.create');
+
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+        $data = new ModelKontak();
+        $data->nama = $request->nama;
+        $data->email = $request->email;
+        $data->nohp = $request->nohp;
+        $data->alamat = $request->alamat;
+        $data->save();
+        return redirect()->route('kontak.index')->with('alert-success','Berhasil Menambahkan Data!');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+        $data = Kontak::where('id',$id)->get();
+
+//        return view('kontak.edit',compact('data'));
+        return compact('data');
+
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+        $data = Kontak::where('id',$id)->get();
+
+        return view('kontak.edit',compact('data'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+        $data = Kontak::where('id',$id)->first();
+        $data->nama = $request->nama;
+        $data->email = $request->email;
+        $data->nohp = $request->nohp;
+        $data->alamat = $request->alamat;
+        $data->save();
+        return redirect()->route('kontak.index')->with('alert-success','Data berhasil diubah!');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+        $data = Kontak::where('id',$id)->first();
+        $data->delete();
+        return redirect()->route('kontak.index')->with('alert-success','Data berhasi dihapus!');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -23,3 +23,7 @@ Route::get('/', function () {
 Route::get('/page2', function () {
     return view('pages.page2');
 });
+
+Route::resource('kontak','KontakController');
+
+
